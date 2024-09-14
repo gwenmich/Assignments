39a08259bc834bd9ab3e477fc292a563dd15eb91
CGMich <79330147+cgmich@users.noreply.github.com>
2024-09-14 19:11:55 +0100

    Created loop function

--- Assignment-2/main.py
+++ Assignment-2/main.py
@@ -13,26 +13,46 @@ def random_game_gen():
     if user_platform.lower() == "pc":
         pc_games_list = []
 
-        for game in database:
-            if game["platform"] == "PC (Windows)":
+    for game in database:
+        if game["platform"] == "PC (Windows)":
                 pc_games_list.append(game["title"])
 
-        pc_game = random.choice(pc_games_list)
+    pc_game = random.choice(pc_games_list)
 
-        for game in database:
-            if pc_game == game["title"]:
-                pc_game_url = game["game_url"]
-                pc_game_year = game["release_date"][0 : 4]
-                pc_game_info = game["short_description"]
-                break
+    for game in database:
+        if pc_game == game["title"]:
+            pc_game_url = game["game_url"]
+            pc_game_year = game["release_date"][0 : 4]
+            pc_game_info = game["short_description"]
+            break
 
-        pc_suggestion = (f"""Based on your platform choice, here is a game that you might enjoy! 
-        \n{pc_game} ({pc_game_year}), url: {pc_game_url}
-        \n{pc_game_info}""")
+    pc_suggestion = (f"""Based on your platform choice, here is a game that you might enjoy! 
+    \n{pc_game} ({pc_game_year}), url: {pc_game_url}
+    \n{pc_game_info}""")
 
 
-        # return pc_suggestion
-        print(pc_suggestion)
+    # return pc_suggestion
+    print(pc_suggestion)
+    loop_suggestion()
+
+
+
+
+def loop_suggestion():
+    continuous = True
+    while continuous:
+        continue_prompt = input("Would you like another suggestion? (Y/N) ")
+        if continue_prompt.lower() == "y" or continue_prompt.lower() == "yes":
+            random_game_gen()
+        elif continue_prompt.lower() == "n" or continue_prompt.lower() == "no":
+            continuous = False
+        else:
+            input("I'm sorry I didn't understand that. Try again.")
+            loop_suggestion()
+    else:
+        # return "Thank you for choosing us for your gaming recommendations. Goodbye!"
+        print("Thank you for choosing us for your gaming recommendations. Goodbye!")
+
 
 
 random_game_gen()
