53026a16632a2eefa40dbfd99ad2425a849f0d2b
CGMich <79330147+cgmich@users.noreply.github.com>
2024-09-14 16:20:10 +0100

    Written pseudocode for app

--- Assignment-2/main.py
+++ Assignment-2/main.py
@@ -5,3 +5,17 @@ endpoint = "https://www.freetogame.com/api/games"
 response = requests.get(endpoint)
 database = response.json()
 
+# app to welcome user
+# to ask if they want a pc or web based game
+# to ask if they would like a specific genre
+# make variable that stores all genres (set)?
+# provide list of genres
+# give suggestion
+
+# def random_game_gen():
+#     print("Welcome to our Free Game suggestion app")
+#     platform = input("")
+# for game in database:
+#     # title = random.choice(list(game.values()))
+#     print(game)
+
