29fe5412dfde1f26ea862266153c0adfdb6e1b49
CGMich <79330147+cgmich@users.noreply.github.com>
2024-09-14 19:30:12 +0100

    Added to random_game_gen function with web choice

--- Assignment-2/main.py
+++ Assignment-2/main.py
@@ -13,33 +13,54 @@ def random_game_gen():
     if user_platform.lower() == "pc":
         pc_games_list = []
 
-    for game in database:
-        if game["platform"] == "PC (Windows)":
+        for game in database:
+            if game["platform"] == "PC (Windows)":
                 pc_games_list.append(game["title"])
 
-    pc_game = random.choice(pc_games_list)
+        pc_game = random.choice(pc_games_list)
 
-    for game in database:
-        if pc_game == game["title"]:
-            pc_game_url = game["game_url"]
-            pc_game_year = game["release_date"][0 : 4]
-            pc_game_info = game["short_description"]
-            break
+        for game in database:
+            if pc_game == game["title"]:
+                pc_game_url = game["game_url"]
+                pc_game_year = game["release_date"][0 : 4]
+                pc_game_info = game["short_description"]
+                break
 
-    pc_suggestion = (f"""Based on your platform choice, here is a game that you might enjoy! 
-    \n{pc_game} ({pc_game_year}), url: {pc_game_url}
-    \n{pc_game_info}""")
+        pc_suggestion = (f"""Based on your platform choice, here is a game that you might enjoy! 
+        \n{pc_game} ({pc_game_year}), url: {pc_game_url}
+        \n{pc_game_info}""")
 
+        return pc_suggestion
+        loop_suggestion()
 
-    # return pc_suggestion
-    print(pc_suggestion)
-    loop_suggestion()
+    elif user_platform.lower() == "web":
+        web_games_list = []
 
+        for game in database:
+            if game["platform"] == "Web Browser":
+                web_games_list.append(game["title"])
+
+        web_game = random.choice(web_games_list)
+
+        for game in database:
+            if web_game == game["title"]:
+                web_game_url = game["game_url"]
+                web_game_year = game["release_date"][0 : 4]
+                web_game_info = game["short_description"]
+                break
+
+        web_suggestion = (f"""Based on your platform choice, here is a game that you might enjoy! 
+        \n{web_game} ({web_game_year}), url: {web_game_url}
+        \n{web_game_info}""")
+
+        return web_suggestion
+        loop_suggestion()
 
 
 
 def loop_suggestion():
     continuous = True
+
     while continuous:
         continue_prompt = input("Would you like another suggestion? (Y/N) ")
         if continue_prompt.lower() == "y" or continue_prompt.lower() == "yes":
@@ -48,10 +69,8 @@ def loop_suggestion():
             continuous = False
         else:
             input("I'm sorry I didn't understand that. Try again.")
-            loop_suggestion()
     else:
-        # return "Thank you for choosing us for your gaming recommendations. Goodbye!"
-        print("Thank you for choosing us for your gaming recommendations. Goodbye!")
+        return "Thank you for choosing us for your gaming recommendations. Goodbye!"
 
 
 
