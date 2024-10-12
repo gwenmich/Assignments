0c58a283ac6425c1c80a9a34f4b5bf1cf976e176
CGMich <79330147+cgmich@users.noreply.github.com>
2024-10-12 13:51:30 +0100

    Configured app file with first route

---
+++ Assignment-4/source/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+from db_utils import get_all_horror_games
+
+app = Flask(__name__)
+
+@app.route("/horror-games", methods = ["GET"])
+def get_all_games():
+    return jsonify(get_all_horror_games())
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
+    print(get_all_games())
